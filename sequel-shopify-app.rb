53ce263dc97037623bba258471df5eb20c28099f
Michael Hewson <michael@michaelhewson.ca>
2017-04-05 15:57:20 -0400

    did a whole bunch of changes; hopefully it works next time

--- sequel-shopify-app.rb
+++ sequel-shopify-app.rb
@@ -5,6 +5,8 @@ gem 'activeresource', git: 'https://github.com/rails/activeresource'
 gem 'shopify_api'
 gem 'shopify_api_mixins', git: 'https://github.com/mikeyhew/shopify_api_mixins'
 gem 'shopify_app'
+gem 'shopify_app_mixins', git: 'https://github.com/mikeyhew/shopify_app_mixins'
+gem 'dotenv-rails'
 
 initializer "shopify_api.rb", <<-CODE
 ShopifyAPI::Connection.retry_on_429
@@ -31,37 +33,45 @@ production:
   url: <%= ENV['DATABASE_URL'] %>
 CODE
 
-initializer "shopify_app.rb", <<-CODE
-ShopifyApp::SessionRepository.send :define_method, :storage do
+def generate_shop_model
+  generate :model, 'shop', 'shopify_domain:string:uniq', 'shopify_token:string', '--no-migration'
+  remove_file 'app/models/shop.rb'
+  file 'app/models/shop.rb', <<-CODE
+require 'shopify_app_mixins/sequel_session_storage'
+class Shop < Sequel::Model
+  include ShopifyAppMixins::SequelSessionStorage
+end
+CODE
+  file "db/migrate/#{Time.now.utc.strftime('%Y%m%d%H%M%S')}_create_shops.rb", <<-CODE
+Sequel.migration do
+  change do
+    create_table :shops do
+      primary_key :id
+      String :shopify_domain, null: false, unique: true
+      String :shopify_token
+    end
+  end
+end
+CODE
+end
+
+def shopify_app_initializer
+<<-CODE
+ShopifyApp::SessionRepository.send :define_singleton_method, :load_storage do
   Shop
 end
 
 ShopifyApp.configure do |config|
+  config.application_name = "My Sequel Shopify App"
   config.embedded_app = true
-  config.api_key = ENV['SHOPIFY_API_KEY']
-  config.secret = ENV['SHOPIFY_SECRET']
+  config.api_key = ENV['SHOPIFY_API_KEY'] || (raise "Missing SHOPIFY_API_KEY")
+  config.secret = ENV['SHOPIFY_SECRET'] || (raise "Missing SHOPIFY_SECRET")
   config.scope = 'write_products, write_orders'
   # webhooks_hostname = ENV['WEBHOOKS_HOSTNAME']
   # raise "Missing WEBHOOKS_HOSTNAME" unless webhooks_hostname.present?
   # config.webhooks = [
-  #   # {topic: 'orders/create', address: "https://\#{ENV['WEBHOOKS_HOSTNAME']}/webhooks/new_order"}
+  #   # {topic: 'orders/create', address: "https://\#{webhooks_hostname}/webhooks/new_order"}
   # ]
-end
-CODE
-
-def generate_shop_model
-  generate :model, 'shop', 'shopify_domain:string:uniq', 'shopify_token:string', '--no-migration'
-  file "db/migrate/#{Time.now.utc.strftime('%Y%m%d%H%M%S')}_create_shops.rb", <<-CODE
-Sequel.migration do
-change do
-
-  create_table :shops do
-    primary_key :id
-    String :shopify_domain, null: false
-    String :shopify_token, null: false
-  end
-
-end
 end
 CODE
 end
@@ -69,6 +79,15 @@ end
 after_bundle do
   run 'bin/spring stop' # sometimes it hangs if you don't do this
   generate_shop_model
+  generate 'shopify_app:install'
+  remove_file 'config/initializers/shopify_session_repository.rb'
+  remove_file 'config/initializers/shopify_app.rb'
+  initializer 'shopify_app.rb', shopify_app_initializer
+  inject_into_file 'config/application.rb', after: "  class Application < Rails::Application\n", "    config.force_ssl"
+  file ".env", <<-CODE
+SHOPIFY_API_KEY=
+SHOPIFY_SECRET=
+CODE
   git :init
   git add: '.'
   git commit: '-m initial'
