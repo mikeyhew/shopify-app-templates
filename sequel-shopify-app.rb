7c8702fd07c99e0688459d6ba99c07baac284596
Michael Hewson <michael@michaelhewson.ca>
2017-04-07 00:47:33 -0400

    Switch to my fork of sequel-rails

--- sequel-shopify-app.rb
+++ sequel-shopify-app.rb
@@ -1,6 +1,6 @@
 gem 'sequel'
 gem 'pg'
-gem 'sequel-rails', git: 'https://github.com/TalentBox/sequel-rails.git'
+gem 'sequel-rails', git: 'https://github.com/mikeyhew/sequel-rails.git'
 gem 'activeresource', git: 'https://github.com/rails/activeresource'
 gem 'shopify_api'
 gem 'shopify_api_mixins', git: 'https://github.com/mikeyhew/shopify_api_mixins'
