eb5706d7a4d2c80a3db897ac5561855e16a25b06
Michael Hewson <michael@michaelhewson.ca>
2017-09-26 16:20:17 -0400

    use ENV.fetch

--- sequel-shopify-app.rb
+++ sequel-shopify-app.rb
@@ -67,11 +67,10 @@ end
 ShopifyApp.configure do |config|
   config.application_name = "My Sequel Shopify App"
   config.embedded_app = true
-  config.api_key = ENV['SHOPIFY_API_KEY'] || (raise "Missing SHOPIFY_API_KEY")
-  config.secret = ENV['SHOPIFY_SECRET'] || (raise "Missing SHOPIFY_SECRET")
+  config.api_key = ENV.fetch 'SHOPIFY_API_KEY'
+  config.secret = ENV.fetch 'SHOPIFY_SECRET'
   config.scope = 'write_products, write_orders'
-  # webhooks_hostname = ENV['WEBHOOKS_HOSTNAME']
-  # raise "Missing WEBHOOKS_HOSTNAME" unless webhooks_hostname.present?
+  # webhooks_hostname = ENV.fetch 'WEBHOOKS_HOSTNAME'
   # config.webhooks = [
   #   # {topic: 'orders/create', address: "https://\#{webhooks_hostname}/webhooks/new_order"}
   # ]
